3e995d5b7b895dcfedd2228aee7fb47edf70c3e7
ax <114583570+bths-ax@users.noreply.github.com>
2023-04-17 08:13:28 -0400

    i love short labs

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String [] args) {
+		System.out.println(sum(6));
+		System.out.println(sum(10));
+	}
+
+	public static int sum(int n) {
+		if (n == 0) return 0;
+		return n + sum(n - 1);
+	}
+}
